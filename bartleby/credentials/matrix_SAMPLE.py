2570a21d462ac6d9ac6e7d28d144d4cc16513df0
George Perdrizet <george@perdrizet.org>
2024-03-07 06:51:54 -0500

    Updated tracking of credentials directory and added sample files so user know what info to add.

---
+++ bartleby/credentials/matrix_SAMPLE.py
@@ -0,0 +1,7 @@
+# This file hold the Matrix server and account info for bartleby. Replace the
+# values with your own and rename the file matrix.py
+
+matrix_room_id = '<MATRIX_ROOM_ID>' # The room to join bartleby to - Note: this room must already exist when starting bartleby
+matrix_server_url = '<MATRIX_SERVER_URL>' # bartleby's homeserver - Note: you need to make an account for him manualy
+matrix_bot_username = 'bartleby'
+matrix_bot_password = '<BARTLEBYS_PASSWORD>' # bartleby's Matrix login password for the above server
