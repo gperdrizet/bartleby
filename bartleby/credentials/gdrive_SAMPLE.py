3f4a69d464400c1dfda514b1ddb66ddcff8be3ae
George Perdrizet <george@perdrizet.org>
2024-03-25 22:50:54 -0400

    Set default gdrive folder to None

--- bartleby/credentials/gdrive_SAMPLE.py
+++ bartleby/credentials/gdrive_SAMPLE.py
@@ -1,4 +1,4 @@
 # If you would like to set a default Google Drive folder rather
 # than setting it via the chat each time, replace <GDRIVE_FOLDER_ID>
 # with your folder ID and an rename this file to gdrive.py
-gdrive_folder_id = '<GDRIVE_FOLDER_ID>'
+gdrive_folder_id = None
