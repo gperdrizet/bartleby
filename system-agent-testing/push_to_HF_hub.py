106336a2add27b1d5988bc0338ff43fe16b6950e
George Perdrizet <george@perdrizet.org>
2024-03-28 22:25:04 -0400

    Finished improved system-agent model and pushed to HF hub.

--- system-agent-testing/push_to_HF_hub.py
+++ system-agent-testing/push_to_HF_hub.py
@@ -6,4 +6,8 @@ os.environ['HF_HOME'] = '/mnt/fast_scratch/huggingface_transformers_cache'
 
 if __name__ == '__main__':
 
-    model = T5ForConditionalGeneration.from_pretrained('2024-03-26_T5-base-system-agent')
+    model = T5ForConditionalGeneration.from_pretrained(
+        '/mnt/fast_scratch/huggingface_transformers_cache/2024-03-26_T5-base-system-agent'
+    )
+
+    model.push_to_hub('T5-base-system-agent')
