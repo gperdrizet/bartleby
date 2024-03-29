0008375257b9838b07d4bab632c7cf0c722bc3a7
George Perdrizet <george@perdrizet.org>
2024-03-28 22:29:31 -0400

    Updated system agent with new T5-base model.

--- bartleby/classes/system_agent_class.py
+++ bartleby/classes/system_agent_class.py
@@ -15,26 +15,29 @@ class System_agent:
         self.logger = logger
 
         # Start tokenizer and models
-        self.tokenizer = T5Tokenizer.from_pretrained('google-t5/t5-small')
+
+        self.system_agent_tokenizer = T5Tokenizer.from_pretrained('google-t5/t5-base')
 
         self.system_agent_model = T5ForConditionalGeneration.from_pretrained(
-            '/mnt/fast_scratch/huggingface_transformers_cache/T5-small-system-agent',
+            'gperdrizet/T5-base-system-agent',
             device_map='cpu'    
         )
 
+        self.output_sizer_tokenizer = T5Tokenizer.from_pretrained('google-t5/t5-small')
+
         self.output_sizer_model = T5ForConditionalGeneration.from_pretrained(
-            '/mnt/fast_scratch/huggingface_transformers_cache/T5-small-output-size-selector',
+            'gperdrizet/T5-small-output-size-selector',
             device_map='cpu'    
         )
 
     def translate_command(self, message):
-        input_ids = self.tokenizer(f'summarize: {message}', return_tensors='pt').input_ids
+        input_ids = self.system_agent_tokenizer(f'summarize: {message}', return_tensors='pt').input_ids
         outputs = self.system_agent_model.generate(input_ids, max_new_tokens=10)
 
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return self.system_agent_tokenizer.decode(outputs[0], skip_special_tokens=True)
     
     def select_output_size(self, message):
-        input_ids = self.tokenizer(f'summarize: {message}', return_tensors='pt').input_ids
+        input_ids = self.output_sizer_tokenizer(f'summarize: {message}', return_tensors='pt').input_ids
         outputs = self.output_sizer_model.generate(input_ids, max_new_tokens=10)
 
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return self.output_sizer_tokenizer.decode(outputs[0], skip_special_tokens=True)
