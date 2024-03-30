d51d962e573b847056881a0dc3bf367f20582498
George Perdrizet <george@perdrizet.org>
2024-03-30 12:07:23 -0400

    Updated bitsandbytes build.

---
+++ bitsandbytes-0.42.0/build/lib/bitsandbytes/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from . import cuda_setup, utils, research
+from .autograd._functions import (
+    MatmulLtState,
+    bmm_cublas,
+    matmul,
+    matmul_cublas,
+    mm_cublas,
+    matmul_4bit
+)
+from .cextension import COMPILED_WITH_CUDA
+from .nn import modules
+
+if COMPILED_WITH_CUDA:
+    from .optim import adam
+
+__pdoc__ = {
+    "libbitsandbytes": False,
+    "optim.optimizer.Optimizer8bit": False,
+    "optim.optimizer.MockArgs": False,
+}
+
+__version__ = "0.42.0"
+
+PACKAGE_GITHUB_URL = "https://github.com/TimDettmers/bitsandbytes"
